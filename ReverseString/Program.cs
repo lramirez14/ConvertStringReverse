1893a1fd3d9729b1c7a2e5d7afa1705ae85881c7
Luis Ramírez <lramirez@cinesunidos.com>
2017-10-15 08:00:50 -0400

    - Added Reverse string project, this is a Console application C#

---
+++ ReverseString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            string result = string.Empty;
+            Console.WriteLine("Inserte un texto: ");
+            string value = Console.ReadLine();
+
+            for (int i = value.Length; i > 0; i--)
+            {
+                result = result + value.Substring(i - 1, 1);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Texto con orden inverso:");
+            Console.WriteLine(result);
+            Console.ReadKey(true);
+        }
+
+        
+    }
+}
